0b13a55e98e2517a7850a6b1bc4ab8a78375625f
bartvanbenthem <bartvanbenthem@hotmail.com>
2025-12-13 13:48:51 +0100

    cleanup utils

--- src/utils.rs
+++ src/utils.rs
@@ -6,52 +6,6 @@ use std::io::{BufWriter, Write};
 use std::path::Path;
 use tracing::*;
 
-use anyhow::Context;
-use tokio::fs;
-use tokio_stream::StreamExt;
-use tokio_stream::wrappers::ReadDirStream;
-
-// Generic function to fetch and write a resource in json format to disk
-pub async fn cleanup_resource_logs(
-    mount_path: &str,
-    cluster_name: &str,
-    retention: &u16,
-    tf: &i64,
-) -> Result<(), anyhow::Error> {
-    let file_path = Path::new(mount_path).join(cluster_name);
-    let retention_secs = (*retention as i64) * 24 * 60 * 60;
-
-    let read_dir = fs::read_dir(&file_path)
-        .await
-        .context(format!("Failed to read directory {:?}", file_path))?;
-
-    let mut entries = ReadDirStream::new(read_dir);
-
-    while let Some(entry_result) = entries.next().await {
-        let entry = entry_result?;
-        let path = entry.path();
-
-        if let Some(folder_name) = path.file_name().and_then(|s| s.to_str()) {
-            match folder_name.parse::<i64>() {
-                Ok(folder_timestamp) => {
-                    let age = tf - folder_timestamp;
-                    if age > retention_secs {
-                        info!("Removing old folder: {:?}", path);
-                        fs::remove_dir_all(&path)
-                            .await
-                            .context(format!("Failed to delete folder {:?}", path))?;
-                    }
-                }
-                Err(_) => {
-                    error!("Skipping non-numeric folder: {:?}", folder_name);
-                }
-            }
-        }
-    }
-
-    Ok(())
-}
-
 /// Writes a list of serializable items to a file in JSONL format.
 pub async fn write_json_to_file<T>(items: &[T], file_name: &str) -> Result<(), anyhow::Error>
 where
